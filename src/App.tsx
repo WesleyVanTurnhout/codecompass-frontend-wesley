55195dfe6925b9aa8f8df86ed6f1d2cf78ffeef3
wesley <wesley_turnhout28@hotmail.com>
2025-09-24 09:48:51 +0200

    Add ProfileComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route, Navigate} from "react-router-dom"
+import { Routes, Route} from "react-router-dom"
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 
@@ -12,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/homepage" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
             </Routes>
         </BrowserRouter>
     </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
+import ProfileComponent from "../components/ProfileComponent";
 
 function HomePage() {
     return (
         <div className="Homepage">
             <h1>Welkom admin</h1>
+            <ProfileComponent />
         </div>
     );
 } export default HomePage;
