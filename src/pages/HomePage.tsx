819a816cbfb68d71a9f5c6cb562ae750c538647e
wesley <wesley_turnhout28@hotmail.com>
2025-09-24 10:23:33 +0200

    Revert "Add ProfileComponent"
    This reverts commit 937077530fe7aa17614dceb7904e267e4f6bb4aa.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,8 @@
-import ProfileComponent from "../components/ProfileComponent";
 
 function HomePage() {
     return (
         <div className="Homepage">
             <h1>Welkom admin</h1>
-            <ProfileComponent />
         </div>
     );
 } export default HomePage;
